3335789a5f21d5bc76da52b189a0faa7cb6f1d43
bou2201 <lamchuc123456@gmail.com>
2024-06-04 13:44:38 +0700

    style: :lipstick: update skills

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Container } from "@mui/material";
-import { skills, tools} from "../../data"
-import ItemSkill from "./ItemSkill";
-import "./skill.scss";
+import React from 'react';
+import { Container } from '@mui/material';
+import { skills, tools } from '../../data';
+import ItemSkill from './ItemSkill';
+import './skill.scss';
 
 const Skill = () => {
   return (
@@ -12,19 +12,17 @@ const Skill = () => {
           Skill <span>sets</span>
         </h1>
         <div className="skill-list">
-          {skills &&
-            skills.map((skill, index) => (
-              <ItemSkill key={index} title={skill.title} image={skill.image} />
-            ))}
+          {skills.map((skill) => (
+            <ItemSkill key={skill.title} title={skill.title} image={skill.image} />
+          ))}
         </div>
-        <h1 className="section-title" style={{paddingTop: 50}}>
+        <h1 className="section-title" style={{ paddingTop: 50 }}>
           <span>Some</span> tools
         </h1>
         <div className="skill-list">
-          {tools &&
-            tools.map((tool, index) => (
-              <ItemSkill key={index} title={tool.title} image={tool.image} />
-            ))}
+          {tools.map((tool) => (
+            <ItemSkill key={tool.title} title={tool.title} image={tool.image} />
+          ))}
         </div>
       </Container>
     </section>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,53 +1,68 @@
-import Html from "../assets/skills/html.png";
-import Css from "../assets/skills/css.png";
-import Sass from "../assets/skills/sass.png";
-import Js from "../assets/skills/js.png";
-import React from "../assets/skills/reactjs.png";
-import Bootstrap from "../assets/skills/bootstrap.png";
-import Mui from "../assets/skills/mui.png";
-import Node from "../assets/skills/nodejs.png";
-import MySql from "../assets/skills/mysql.png";
-import Mongo from "../assets/skills/mongodb.png";
+import Html from '../assets/skills/html.png';
+import Css from '../assets/skills/css.png';
+import Sass from '../assets/skills/sass.png';
+import Js from '../assets/skills/js.png';
+import Ts from '../assets/skills/typescript.png';
+import React from '../assets/skills/reactjs.png';
+import Redux from '../assets/skills/redux.png';
+import Tailwindcss from '../assets/skills/tailwind-css.png';
+import Bootstrap from '../assets/skills/bootstrap.png';
+import Mui from '../assets/skills/mui.png';
+import Node from '../assets/skills/nodejs.png';
+import MySql from '../assets/skills/mysql.png';
+import Mongo from '../assets/skills/mongodb.png';
 
 export const skills = [
   {
     image: Html,
-    title: "Html",
+    title: 'Html',
   },
   {
     image: Css,
-    title: "Css",
+    title: 'Css',
   },
   {
     image: Sass,
-    title: "Sass",
+    title: 'Sass',
   },
   {
     image: Js,
-    title: "Javascript",
+    title: 'Javascript',
+  },
+  {
+    image: Ts,
+    title: 'Typescript',
   },
   {
     image: React,
-    title: "React JS",
+    title: 'React JS',
+  },
+  {
+    image: Redux,
+    title: 'Redux',
+  },
+  {
+    image: Tailwindcss,
+    title: 'TailwindCSS',
   },
   {
     image: Bootstrap,
-    title: "Bootstrap",
+    title: 'Bootstrap',
   },
   {
     image: Mui,
-    title: "Material UI",
+    title: 'Material UI',
   },
   {
     image: Node,
-    title: "Node JS",
+    title: 'Node JS',
   },
   {
     image: MySql,
-    title: "My SQL",
+    title: 'My SQL',
   },
   {
     image: Mongo,
-    title: "Mongo DB",
+    title: 'Mongo DB',
   },
 ];
